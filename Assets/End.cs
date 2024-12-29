ab5dc70bbfe6e7286e5d584887cabdb3a34e59fe
stanLy490 <321684298@qq.com>
2024-12-29 11:25:21 +0800

    12/28
    第一次build

---
+++ Assets/End.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour
+{
+
+    public GameObject SFX_1;
+    public GameObject SFX_2;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SFX_1.SetActive(true);
+            SFX_2.SetActive(true);
+        }
+    }
+}
